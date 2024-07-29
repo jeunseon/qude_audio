876cc3330cdce2f9c0ec8b91afdbe42620da255e
jeunseon <euns420@naver.com>
2024-07-29 19:20:42 +0900

    gsap scrollTrigger timeline gsap.to()

--- js/main.js
+++ js/main.js
@@ -7,5 +7,31 @@ window.onload = function(){
     menuOpen.addEventListener('click', () => {
         menuBox.classList.toggle('on');
     });
-}
 
+
+    // scrollTrigger 
+    // GSAP 라이브러리의 스크롤트리거를 등록
+    gsap .registerPlugin(ScrollTrigger);
+
+    // 01. visual
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.visual',
+            start: '100% 100%',
+            // .visual의 100%  브라우저의 100%
+            end: '100% 0%',
+            scrub: 1,
+            // scrub은 ScrollTrigger의 이벤트가 스크롤이 사용될때만 재생되도록 만들어주는 속성 (안적으면 트리거시점 나오면 스크롤안해도 계속 애니 진행됨)
+            // scrub은 true 나 숫자로 값을 써줄수 있는데 true 같은 경우는 스크롤하면 애니 바로 멈추고 숫자는 그 시점을 따라잡는데 N초가 걸려서 애니가 더 부드러움
+            // markers: true
+        }
+    })
+    .to('.logoWrap #j', {x:-150, y:250, rotate:20, ease:'none', duration:5,}, 0)
+    .to('.logoWrap #y', {x:-30, y:150, rotate:-10, ease:'none', duration:5,}, 0)
+    .to('.logoWrap #o', {x:0, y:400, rotate:-10, ease:'none', duration:5,}, 0)
+    .to('.logoWrap #u', {x:50, y:300, rotate:10, ease:'none', duration:5,}, 0)
+    .to('.logoWrap #n', {x:100, y:100, rotate:-10, ease:'none', duration:5,}, 0)
+    .to('.logoWrap #g', {x:50, y:450, rotate:20, ease:'none', duration:5,}, 0)
+
+    
+}
